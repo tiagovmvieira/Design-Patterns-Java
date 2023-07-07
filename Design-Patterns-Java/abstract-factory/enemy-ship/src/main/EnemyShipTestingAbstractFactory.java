72500cbb4807c2e631b5f1a99d628836a84522f4
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-07-07 22:23:10 +0100

    Update EnemyShipTestingAbstract Factory demo to expose an Enemy Ship Example

--- Design-Patterns-Java/abstract-factory/enemy-ship/src/main/EnemyShipTestingAbstractFactory.java
+++ Design-Patterns-Java/abstract-factory/enemy-ship/src/main/EnemyShipTestingAbstractFactory.java
@@ -4,6 +4,13 @@ public class EnemyShipTestingAbstractFactory {
 
     public static void main(String[] args){
 
+        EnemyShipBuilding makeUFOs = new UFOEnemyShipBuilding();
+
+        EnemyShipAbstractFactory theGrunt = makeUFOs.orderTheShip("UFO");
+        System.out.println(theGrunt + "\n");
+
+        EnemyShipAbstractFactory theBoss = makeUFOs.orderTheShip("UFO BOSS");
+        System.out.println(theBoss + "\n");
 
     }
 }
