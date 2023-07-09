7d33da74841b9d73052706ca6cff2255a18cc817
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-07-09 21:08:57 +0100

    Add Command Design Pattern simplest example exposed on Head First Design Patterns book

---
+++ Design-Patterns-Java/command/demo/src/main/RemoteControlTest.java
@@ -0,0 +1,21 @@
+package main;
+
+public class RemoteControlTest {
+
+    public static void main(String[] args){
+        SimpleRemoteControl remote = new SimpleRemoteControl();
+
+        Light light = new Light();
+        GarageDoor garageDoor = new GarageDoor();
+
+        LightOnCommand lightOn = new LightOnCommand(light);
+        GarageDoorUpCommand garageOpen = new GarageDoorUpCommand(garageDoor);
+
+        remote.setCommand(lightOn);
+        remote.buttonWasPressed();
+
+        remote.setCommand(garageOpen);
+        remote.buttonWasPressed();
+
+    }
+}
