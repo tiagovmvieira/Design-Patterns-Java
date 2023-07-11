cd2bd460b9909e2cca423c90f1b31d69aea434cf
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-07-11 22:48:42 +0100

    Add classes used on the demo of the Adapter Design Pattern

---
+++ Design-Patterns-Java/adapter/enemy-war/src/main/EnemyAttacker.java
@@ -0,0 +1,9 @@
+package main;
+
+public interface EnemyAttacker {
+
+    public void fireWeapon();
+    public void driveForward();
+    public void assignDriver(String driverName);
+
+}
---
+++ Design-Patterns-Java/adapter/enemy-war/src/main/EnemyRobotAdapter.java
@@ -0,0 +1,30 @@
+package main;
+
+public class EnemyRobotAdapter implements EnemyAttacker{
+
+    EnemyRobot theRobot;
+
+    public EnemyRobotAdapter(EnemyRobot newRobot){
+
+        this.theRobot = newRobot;
+    }
+
+    @Override
+    public void fireWeapon() {
+
+        theRobot.smashWithHands();
+    }
+
+    @Override
+    public void driveForward() {
+
+        theRobot.walkForward();
+    }
+
+    @Override
+    public void assignDriver(String driverName) {
+
+        theRobot.reactToHuman(driverName);
+    }
+
+}
