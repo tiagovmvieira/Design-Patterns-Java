09b50e63c147308dac56cfdf76ba2215c154919f
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-07-30 22:53:02 +0100

    Add Builder Design Pattern classes exposed on a Robots Builder example

---
+++ Design-Patterns-Java/builder/robots/src/main/RobotEnginner.java
@@ -0,0 +1,25 @@
+package main;
+
+public class RobotEnginner {
+
+    private RobotBuilder robotBuilder;
+
+    public RobotEnginner(RobotBuilder robotBuilder){
+
+        this.robotBuilder = robotBuilder;
+    }
+
+    public Robot getRobot(){
+
+        return this.robotBuilder.getRobot();
+    }
+
+    public void makeRobot(){
+
+        this.robotBuilder.buildRobotHead();
+        this.robotBuilder.buildRobotTorso();
+        this.robotBuilder.buildRobotArms();
+        this.robotBuilder.buildRobotLegs();
+    }
+
+}
---
+++ Design-Patterns-Java/builder/robots/src/main/RobotPlan.java
@@ -0,0 +1,9 @@
+package main;
+
+public interface RobotPlan {
+
+    public void setRobotHead(String head);
+    public void setRobotTorso(String torso);
+    public void setRobotArms(String arms);
+    public void setRobotLegs(String legs);
+}
