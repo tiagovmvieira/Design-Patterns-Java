968bc61bff8b775e20a7212169cd44750a376c8d
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-08-04 17:51:19 +0100

    Add MVC Design Pattern demo TestDrive

---
+++ Design-Patterns-Java/mvc/demo/src/main/DJTestDrive.java
@@ -0,0 +1,11 @@
+package main;
+
+public class DJTestDrive {
+
+    public static void main(String[] args){
+
+        BeatModelInterface model = new BeatModel();
+        ControllerInterface controller = new BeatController(model);
+    }
+
+}
